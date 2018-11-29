d63853dbce2acc5799f35a2f3b88020499091317
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-11-29 18:35:54 +0000

    Schema corrected and report added

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
---1) --works
+--1) 
 SELECT numprocessosocorro FROM aciona 
 	Group BY numprocessosocorro HAVING COUNT(nummeio) >= ALL(SELECT COUNT(nummeio) 
 		FROM aciona GROUP BY numprocessosocorro); 
 
---2 -- works
+--2 
 SELECT nomeentidade FROM eventoemergencia NATURAL JOIN aciona
   WHERE instantechamada <= '2018-09-23 23:59:59' AND instantechamada >= '2018-06-21 00:00:00'
   GROUP BY nomeentidade HAVING COUNT(numprocessosocorro) >= ALL
@@ -11,22 +11,22 @@ SELECT nomeentidade FROM eventoemergencia NATURAL JOIN aciona
   	WHERE instantechamada <= '2018-09-23 23:59:59' AND instantechamada >= '2018-06-21 00:00:00'
   	GROUP BY nomeentidade);
 
---3) -- works
+--3) 
 SELECT numprocessosocorro 
 FROM eventoemergencia 
 NATURAL JOIN aciona 
 WHERE moradalocal = 'Oliveira do Hospital' AND instantechamada >= '2018-01-01 00:00:00' AND instantechamada <= '2018-12-31 23:59:00'
 AND numprocessosocorro NOT IN(SELECT numprocessosocorro FROM audita);
 
---4) --works
+--4) 
 SELECT COUNT(numsegmento) FROM segmentovideo 
 NATURAL JOIN video NATURAL JOIN vigia 
 WHERE duracao > 60 AND moradalocal = 'Monchique' AND datahorafim <= '2018-08-31 23:59' AND datahorainicio >= '2018-08-01 00:00';
 
---5) --works
+--5)
 SELECT nummeio, nomeentidade FROM meiocombate EXCEPT(SELECT nummeio, nomeentidade FROM alocado GROUP BY nummeio, nomeentidade);
 
---6) --works
+--6)  
 SELECT nomeentidade 
 FROM meiocombate NATURAL JOIN aciona WHERE NOT EXISTS 
 (SELECT numprocessosocorro FROM processosocorro EXCEPT SELECT numprocessosocorro FROM aciona);
--- schema.sql
+++ schema.sql
@@ -21,11 +21,16 @@ drop table if exists Coordenador cascade;
 drop table if exists Meio cascade;
 drop table if exists EntidadeMeio cascade;
 
+-- Camara (numCamara)
+-- numCamara is primary key
 create table Camara
     (numCamara int not null unique,
     constraint pk_camara primary key(numCamara)
 );
 
+-- Video (dataHoraInicio, dataHoraFim, numCamara)
+-- numCamara is a foreign key from Camara
+-- dataHoraInicio is primary key
 create table Video
     (dataHoraInicio timestamp not null,
     dataHoraFim timestamp not null,
@@ -35,21 +40,30 @@ create table Video
     constraint chk_video_time check (dataHoraFim > dataHoraInicio)
 );
 
+-- Segmento (numSegmento, duracao, dataHoraInicio, numCamara)
+-- numCamara is a foreign key from Camara
+-- dataHoraInicio is a foreign key from Video
+-- (numCamara, numSegmento, dataHoraInicio) is primary key
 create table SegmentoVideo
     (numSegmento int not null ,
     duracao int not null,
     dataHoraInicio timestamp not null,
     numCamara int not null ,
     constraint pk_segment primary key (numCamara, numSegmento, dataHoraInicio),
-    constraint fk_camara foreign key (numCamara) references Camara(numCamara),
+    constraint fk_camara foreign key (numCamara) references Video(numCamara),
     constraint fk_dataHoraInicio foreign key (dataHoraInicio) references Video(dataHoraInicio)
 );
 
+-- Locais (moradaLocal)
+-- moradaLocal is primary key
 create table Locais
 	(moradaLocal varchar(255) not null unique,
 	constraint pk_local primary key (moradaLocal)
 );
 
+-- Vigia (moradaLocal, numCamara)
+-- moradaLocal is a foreign key from Locais
+-- numCamara is a foreign key from Camara
 create table Vigia
 	(moradaLocal varchar(255) not null,
 	numCamara int not null unique,
@@ -57,28 +71,38 @@ create table Vigia
 	constraint fk_camara foreign key (numCamara) references Camara(numCamara)
 );
 
+-- ProcessoSocorro
+-- numProcessoSocorro is primary key
 create table ProcessoSocorro
 	(numProcessoSocorro int not null unique,
 	constraint pk_numProcessoSocorro primary key (numProcessoSocorro)
-	--verificar restricao para todos os processo estarem associados a um ou mais evento de emergencia */
 );
 
+-- EventoEmergencia
+-- moradaLocal is a foreign key from Locais
+-- numProcessoSocorro is a foreign key from ProcessoSocorro
+-- (numTelefone, nomePessoa) is primary key
 create table EventoEmergencia
 	(numTelefone char(9) not null,
 	instanteChamada timestamp not null,
 	nomePessoa varchar(30) not null,
 	moradaLocal varchar(255) not null,
-	numProcessoSocorro int,
+	numProcessoSocorro int not null,
 	constraint pk_witness primary key(numTelefone, nomePessoa),
 	constraint fk_local foreign key (moradaLocal) references Locais(moradaLocal),
 	constraint fk_numProcessoSocorro foreign key (numProcessoSocorro) references ProcessoSocorro(numProcessoSocorro)
 );
 
+-- EntidadeMeio
+-- nomeEntididade is primary key
 create table EntidadeMeio
 	(nomeEntidade varchar(20) not null unique,
 	constraint pk_nomeEntidade primary key (nomeEntidade)
 );
 
+-- Meio
+-- nomeEntidade is foreign key from EntidadeMeio
+-- (numMeio, nomeEntidade) is primary key
 create table Meio
 	(numMeio int not null,
 	nomeMeio varchar(30) not null,
@@ -87,6 +111,9 @@ create table Meio
 	constraint fk_nomeEntidade foreign key (nomeEntidade) references EntidadeMeio(nomeEntidade)
 );
 
+-- MeioCombate
+-- (numMeio, nomeEntidade) is foreign key from Meio
+-- (numMeio, nomeEntidade) is primary key
 create table MeioCombate
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
@@ -94,6 +121,9 @@ create table MeioCombate
 	constraint fk_meio foreign key (numMeio, nomeEntidade) references Meio(numMeio, nomeEntidade)
 );
 
+-- MeioApoio
+-- (numMeio, nomeEntidade) is foreign key from Meio
+-- (numMeio, nomeEntidade) is primary key
 create table MeioApoio
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
@@ -101,14 +131,19 @@ create table MeioApoio
 	constraint fk_meio foreign key (numMeio, nomeEntidade) references Meio(numMeio, nomeEntidade)
 );
 
+-- MeioSocorro
+-- (numMeio, nomeEntidade) is foreign key from Meio
+-- (numMeio, nomeEntidade) is primary key
 create table MeioSocorro
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
 	constraint fk_meioSocorro primary key (numMeio, nomeEntidade),
 	constraint fk_meio foreign key (numMeio, nomeEntidade) references Meio(numMeio, nomeEntidade)
-
 );
 
+-- Transporta
+-- (numMeio, nomeEntidade) is foreign key from MeioCombate
+-- numProcessoSocorro is foreign key from ProcessoSocorro
 create table Transporta
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
@@ -118,6 +153,9 @@ create table Transporta
 	constraint numProcessoSocorro foreign key (numProcessoSocorro) references ProcessoSocorro(numProcessoSocorro)
 );
 
+-- Alocado
+-- (numMeio, nomeEntidade) is foreign key from MeioApoio
+-- numProcessoSocorro is foreign key from ProcessoSocorro
 create table Alocado
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
@@ -127,6 +165,10 @@ create table Alocado
 	constraint numProcessoSocorro foreign key (numProcessoSocorro) references ProcessoSocorro(numProcessoSocorro)
 );
 
+-- Aciona
+-- (numMeio, nomeEntidade) is foreign key from Meio
+-- numProcessoSocorro is foreign key from ProcessoSocorro
+-- (numMeio, nomeEntidade, numProcessoSocorro) is primary key
 create table Aciona
 	(numMeio int not null,
 	nomeEntidade varchar(20) not null,
@@ -136,11 +178,16 @@ create table Aciona
 	constraint numProcessoSocorro foreign key (numProcessoSocorro) references ProcessoSocorro(numProcessoSocorro)
 );
 
+-- Coordenador
+-- idCoordenador is primary key
 create table Coordenador
 	(idCoordenador int not null unique ,
 	constraint idCoordenador primary key (idCoordenador)
 );
 
+-- Audita
+-- (numMeio, nomeEntidade, numProcessoSocorro) is foreign key from Aciona
+-- idCoordenador is foreign key from Coordenador
 create table Audita
 	(idCoordenador int not null,
 	numMeio int not null,
@@ -153,9 +200,13 @@ create table Audita
 	constraint numMeio foreign key (numMeio, nomeEntidade, numProcessoSocorro) references Aciona(numMeio, nomeEntidade, numProcessoSocorro),
 	constraint idCoordenador foreign key (idCoordenador) references Coordenador(idCoordenador),
 	constraint chk_audit_time check (datahoraInicio < datahoraFim),
-	constraint chk_audit_date check (dataAuditoria <= current_date) /*VERIFICAR*/
+	constraint chk_audit_date check (dataAuditoria <= current_date)
 );
 
+-- Solicita
+-- idCoordenador is foreign key from Coordenador
+-- dataHoraInicioVideo is foreign key from Video
+-- numCamara is foreign key from Camara
 create table Solicita
 	(idCoordenador int not null,
 	dataHoraInicioVideo timestamp not null,
