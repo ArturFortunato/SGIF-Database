da254918956c8d9a5c4179c1ec400577ec415731
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-11-29 11:46:53 +0000

    Updated queries

--- queries.sql
+++ queries.sql
@@ -1,20 +1,24 @@
---1)
-SELECT numprocessosocorro FROM acciona Group BY numprocessosocorro HAVING COUNT(nummeio) >= ALL(SELECT COUNT(nummeio) FROM acciona GROUP BY numprocessosocorro); --works
+--1) --works
+SELECT numprocessosocorro FROM aciona 
+	Group BY numprocessosocorro HAVING COUNT(nummeio) >= ALL(SELECT COUNT(nummeio) 
+		FROM aciona GROUP BY numprocessosocorro); 
 
---2) --doesnt compile
-SELECT nomeentidade FROM audita GROUP BY nomeentidade WHERE datahorafim <= '2018-09-23 23:59:00' AND datahorainicio >= '2018-06-21 00:00:00' HAVING COUNT(numprocessosocorro) >= ALL(SELECT COUNT(numprocessosocorro) FROM audita GROUP BY nomeentidade WHERE datahorafim <= '2018-09-23 23:59:00' AND datahorainicio >= '2018-06-21 00:00:00');
+--2 -- works
+SELECT nomeentidade FROM eventoemergencia NATURAL JOIN aciona
+  WHERE instantechamada <= '2018-09-23 23:59:59' AND instantechamada >= '2018-06-21 00:00:00'
+  GROUP BY nomeentidade HAVING COUNT(numprocessosocorro) >= ALL
+  (SELECT COUNT(numprocessosocorro) FROM eventoemergencia NATURAL JOIN aciona
+  	WHERE instantechamada <= '2018-09-23 23:59:59' AND instantechamada >= '2018-06-21 00:00:00'
+  	GROUP BY nomeentidade);
 
---3) -- compiles but wee dont know if it works // misses unique
-SELECT numprocessosocorro FROM eventoemergencia NATURAL JOIN audita NATURAL JOIN aciona WHERE idcoordenador = NULL AND moradalocal = 'Oliveira do Hospital' AND instantechamada >= '2018-01-01 00:00:00' AND instantechamada <= '2018-12-31 23:59:00';
+--3) -- doesnt work
+SELECT numprocessosocorro FROM eventoemergencia NATURAL JOIN audita NATURAL JOIN aciona WHERE moradalocal = 'Oliveira do Hospital' AND instantechamada >= '2018-01-01 00:00:00' AND instantechamada <= '2018-12-31 23:59:00';
 
---4)
-SELECT COUNT(numSegmento) FROM SegmentoVideo NATURAL JOIN Video NATURAL JOIN Vigia
-  WHERE duracao > 60, moradaLocal = "Monchique" AND dataHoraFim <= "31/08/2018 23:59" AND dataHoraInicio => "01/08/2018 00:00"
+--4) --works
+SELECT COUNT(numsegmento) FROM segmentovideo NATURAL JOIN video NATURAL JOIN vigia WHERE duracao > 60 AND moradalocal = 'Monchique' AND datahorafim <= '2018-08-31 23:59' AND datahorainicio >= '2018-08-01 00:00';
 
---5)
-SELECT numMeio FROM MeioCombate
-  WHERE numMeio NOT IN (SELECT numMeio FROM MeioApoio NATURAL JOIN Acciona)
+--5) --works somewhat but needs fix
+SELECT * FROM meiocombate WHERE nummeio NOT IN (SELECT nummeio FROM meioapoio NATURAL JOIN aciona);
 
---6)
-SELECT nomeEntidade FROM Acciona NATURAL JOIN MeioCombate GROUP BY nomeEntidade
-  HAVING SUM(numProcessoSocorro) >= SELECT SUM(numProcessoSocorro) FROM ProcessoSocorro
+--6) --works
+SELECT nomeentidade FROM aciona NATURAL JOIN meiocombate GROUP BY nomeentidade HAVING SUM(numprocessosocorro) >= (SELECT SUM(numprocessosocorro) FROM processosocorro);
